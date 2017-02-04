6ac3e336368d711889df79799755166556d24c6e
skelstar <skelstar@gmail.com>
2017-02-04 20:16:19 +1300

    A bit broken... going to add EventManager to do flashing leds.

--- arduino-bottle-filler-client.ino
+++ arduino-bottle-filler-client.ino
@@ -1,46 +1,107 @@
-#include "TM1637.h"
-#include "appconfig.h"
+#include <myWifiHelper.h>
+#include <SevenSegmentTM1637.h>
+#include <SevenSegmentExtended.h>
 
-#define CLK 0//pins definitions for TM1637 and can be changed to other ports       
+char versionText[] = "MQTT Bottle Feeder Client v0.9.0";
+
+// WIFI ----------------------------------------
+
+#define FEEDBOTTLE_FEED  "/dev/bottleFeed"
+
+#define WIFI_OTA_NAME   "arduino-bottle-filler-client"
+#define WIFI_HOSTNAME   "arduino-bottle-filler-client"
+
+MyWifiHelper wifiHelper(WIFI_HOSTNAME);
+
+// Clock ----------------------------------------
+
+#define MED_BRIGHT      30
+#define LOW_BRIGHT      15
+
+char TimeDisp[] = "--:--";
+
+#define CLK 0
 #define DIO 2
 
-TM1637 tm1637(CLK,DIO);
+SevenSegmentExtended sevenSeg(CLK,DIO);
 
-#define BRIGHT_DAYLIGHT		5
+volatile int hour = 0;
+volatile int minute = 0;
+
+volatile bool callback_event;
+
+// ----------------------------------------------
+
+void bottlefeed_callback(byte* payload, unsigned int length) {
+
+	if (payload[0] == '-') {
+		hour = -1;
+	} else {
+	    hour = (payload[0]-'0') * 10;
+	    hour += payload[1]-'0';
+	    minute = (payload[3]-'0') * 10;
+	    minute += payload[4]-'0';
+	}
+	callback_event = true;
+}
+
+// ----------------------------------------------
 
 void setup()
 {
 	Serial.begin(9600);
-	delay(100);
-	Serial.println("Ready");
+	delay(50);
+	Serial.println("Booting");
+	Serial.println(versionText);
 
-	tm1637.init();
-	tm1637.set(BRIGHT_DAYLIGHT);	//BRIGHT_TYPICAL = 2,BRIGHT_DARKEST = 0,BRIGHTEST = 7;
+	sevenSeg.begin();
+	sevenSegClear();
+
+    wifiHelper.setupWifi();
+    wifiHelper.setupOTA(WIFI_OTA_NAME);
+
+    wifiHelper.setupMqtt();
+
+    wifiHelper.mqttAddSubscription(FEEDBOTTLE_FEED, bottlefeed_callback);
 }
 
 void loop()
 {
-	int8_t NumTab[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};//0~9,A,b,C,d,E,F
-	int8_t ListDisp[4];
-	unsigned char i = 0;
-	unsigned char count = 0;
-	delay(150);
-	while(1)
-	{
-		i = count;
-		count ++;
-		if(count == sizeof(NumTab)) 
-			count = 0;
-		for(unsigned char BitSelect = 0;BitSelect < 4;BitSelect ++)
-		{
-			ListDisp[BitSelect] = NumTab[i];
-			i ++;
-			if(i == sizeof(NumTab)) i = 0;
-		}
-		tm1637.display(0,ListDisp[0]);
-		tm1637.display(1,ListDisp[1]); 
-		tm1637.display(2,ListDisp[2]);
-		tm1637.display(3,ListDisp[3]);
-		delay(300);
-	}
+    wifiHelper.loopMqtt();
+
+    ArduinoOTA.handle();
+
+    if (callback_event) {
+    	if (hour >= 0) {
+    		sevenSegDisplayTime();
+    	} else {
+			clearTimeDisp();
+	    	sevenSeg.print("----");
+    	}
+    	Serial.println("callback!");
+    	callback_event = false;
+    }
+
+    delay(10);
+}
+
+//------------------------------------------------------------------
+
+void clearTimeDisp() {
+    TimeDisp[0] = '-';
+    TimeDisp[1] = '-';
+    TimeDisp[2] = ':';
+    TimeDisp[3] = '-';
+    TimeDisp[4] = '-';
+}
+
+void sevenSegClear() {
+    sevenSeg.setBacklight(MED_BRIGHT);
+    sevenSeg.print("----");
+    clearTimeDisp();
+}
+
+void sevenSegDisplayTime() {
+    sevenSeg.setBacklight(MED_BRIGHT);
+    sevenSeg.printTime(hour, minute, true);
 }
